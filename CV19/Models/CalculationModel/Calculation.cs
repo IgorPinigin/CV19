e91f8b5a7276a41afe5eec3e0c9b26daf2303ccf
Igor Pinigin <igorpinigin557@gmail.com>
2023-04-02 23:41:31 +0300

    добавлена модель розы ветров

--- CV19/Models/CalculationModel/Calculation.cs
+++ CV19/Models/CalculationModel/Calculation.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using CV19.Models.RoseWindModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
+
 
 namespace CV19.Models.CalculationModel
 {
     class Calculation
     {
+        public Calculation() 
+        {
+            
+        }
+
+        double radius_min = 0.005;
+
+        public double GetRoseWind(Tuple<double, double> cent_p, Tuple<double, double> p)
+        {
+            double fi = RoseWind.Ang(cent_p.Item1, cent_p.Item2, p.Item1, p.Item2);
+            double rose = rs_w.Rose(fi) / rs_w.AntiRose("NW");
+            return rose;
+        }
+
+        public double RModel(double x, double xi, double y, double yi, double radius_max) 
+        {            
+            double radius = Math.Sqrt(Math.Pow((x - xi), 2) + Math.Pow((y - yi), 2));
+            if (radius < radius_min)
+                return 1 / (Math.Pow(radius, 2) + 2 * radius_max * radius + 2 * Math.Pow(radius_max, 2));
+            return 1 / (Math.Pow(radius, 2) * Math.Pow(Math.E, (radius_max * 2 / radius)) * GetRoseWind(new Tuple<double, double>(xi, yi), new Tuple<double, double>(x, y))); 
+        }
+
+        public double ConcInPnt(Tuple<double, double> pnt, double[,] src_pnt, double radius_max = 0.5, double tt = 1.0)
+        {
+            double conc = 0.0;
+            for (int i = 0; i < src_pnt.GetLength(0); i++)
+            {
+                double rModelValue = RModel(pnt.Item1, pnt.Item2, src_pnt[i, 0], src_pnt[i, 1], radius_max);
+                conc += tt * rModelValue;
+            }
+            return conc;
+        }
+
+        public double LpInPoint(Tuple<double, double> pnt, List<Tuple<double[,], double>> src_pnt, double[,] a, double[] b, double mix = 1.0)
+        {
+            List<double> c_arr = new();
+            foreach (Tuple<double[,], double> el in src_pnt)
+            {
+                double conc = mix * ConcInPnt(pnt, el.Item1, el.Item2);
+                c_arr.Add(conc);
+            }
+
+            
+
+            
+
+            return /*чт-то*/ ?? -1;
+        }
+        //def get_rw(cent_p, p):
+        //    fi = self.rs_w.ang(cent_p[0], cent_p[1], p[0], p[1])
+        //    rose = self.rs_w.rose(fi) / self.rs_w.anti_rose("NW")
+        //    return rose
     }
 }
---
+++ CV19/Models/RoseWindModel/RoseWind.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CV19.Models.RoseWindModel
+{
+    class RoseWind
+    {
+        public RoseWind() 
+        {
+        }   
+        public double Ang(Tuple<double, double> cent_p, Tuple<double, double> p) 
+        {
+            double Rs = Math.Atan((p.Item2 - cent_p.Item2) / (p.Item1 - cent_p.Item1));
+            if ((p.Item1 - cent_p.Item1) < 0) 
+            {
+                Rs += Math.PI;
+            }
+            if (Rs < 0)
+            {
+                Rs += Math.PI*2;
+            }
+            return Rs;
+        }
+        public double Rose(double Fi)
+        {
+            string Json;
+
+            using (StreamReader sr = File.OpenText(input_file))
+            {
+                Json = sr.ReadToEnd();
+            }
+
+            dynamic Obj = JsonConvert.DeserializeObject(Json);
+            dynamic RoseW = Obj;
+
+            if (Fi < 0)
+            {
+                Fi += 2 * Math.PI;
+            }
+            if (Fi > 2 * Math.PI)
+            {
+                Fi -= 2 * Math.PI;
+            }
+
+            double DlAng = 2 * Math.PI / 8;
+            List<double> Ff = new List<double>();
+
+            for (int i = 1; i < 9; i++)
+            {
+                Ff.Add(i * DlAng);
+            }
+
+            string[] Sw = new string[] { "W", "NW", "N", "NE", "E", "SE", "S", "SW" };
+            List<int> SortRW = new List<int>();
+
+            foreach (string key in Sw)
+            {
+                int value;
+                if (RoseW.TryGetValue(key, out value))
+                {
+                        SortRW.Add(value);
+                }
+            }
+
+            double Rs = 0.0;
+
+            for (int i = 6; i >= 0; i--)
+            {
+                double F = (i + 1) * DlAng;
+                if (Fi <= F)
+                {
+                    double Rs1 = (SortRW[i] * (Fi - (F - DlAng)) + SortRW[(i + 1) % 8] * (F - Fi)) / DlAng;
+                    Rs = Rs1;
+                    break;
+                }
+            }
+
+            return Rs;
+        }
+    }
+}
